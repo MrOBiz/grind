072f136b3a46c883c2013d4ea9fb75ecb287d368
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-08-19 10:58:19 +0200

    Added obj exos

--- javascript-info-exos/fundamentals.js
+++ javascript-info-exos/fundamentals.js
@@ -1,1 +1,23 @@
 "use strict";
+
+//ARROW FUNCTIONS
+
+let ask = (question, yes, no) => (question.length > 10)? yes() : no();
+
+ask("Do you agree?", () => console.log("Agreed."), () => console.log("Nope."));
+
+//OBJECTS
+
+let user = {};
+
+user.name = "John";
+user.surname = "Smith";
+console.log(user.name);
+console.log(user.surname);
+
+
+user.name = "Pete";
+console.log(user.name);
+
+delete user.name;
+console.log(user.name);
